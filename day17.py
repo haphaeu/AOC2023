a4fef351031820698b5e8937f93165a442dad822
Rafael Rossi <rafael.rossi@akerbp.com>
2023-12-29 20:29:34 +0100

    Day 17 - more cheat :/ Dijkstra alone didn't do it.

--- day17.py
+++ day17.py
@@ -1,3 +1,57 @@
+import numpy as np
+from scipy.sparse.csgraph import dijkstra
+from scipy.sparse import csr_matrix
+
+
+"""
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csgraph.dijkstra.html
+https://stackoverflow.com/questions/53074947/examples-for-search-graph-using-scipy
+
+Build a graph matrix of the grid, with connectivity and distances
+between nodes. The matrix item (i, j) is the distance between 
+nodes i and j. Note that directionality is important, ie:
+
+   distance(i, j) != distance(j, i)
+
+Example:
+
+    grid
+    241
+    321                           to node j
+    325                       0 1 2 3 4 5 6 7 8
+       >4     >1            +-----------------
+  (0)----(1)----(2)       0 | 0 4 0 3 0 0 0 0 0
+   |  <2  |  <4  |      f 1 | 2 0 1 0 2 0 0 0 0
+ v3|^2  v2|^4  v1|^1    r 2 | 0 4 0 0 0 1 0 0 0
+   |  >2  |  >1  |      o 3 | 2 0 0 0 2 0 3 0 0
+  (3)----(4)----(5)     m 4 | 0 4 0 3 0 1 0 2 0
+   |  <3  |  <2  |        5 | 0 0 1 0 2 0 0 0 5
+ v3|^3  v2|^2  v5|^1    i 6 | 0 0 0 3 0 0 0 2 0
+   |  >2  |  >5  |        7 | 0 0 0 0 2 0 3 0 5
+  (6)----(7)----(8)       8 | 0 0 0 0 0 1 0 2 0
+      <3     <2
+
+For convenience, for a grid of size N, it may be better to call
+nodes (r,c), such that connectivity is:
+
+   (r,c) -> (r+1,c), (r,c+1), (r-1,c), (r,c-1)
+   given r+1,c+1 < N, r-1,c-1 >= 0
+
+and then numbering the nodes i as:
+
+    i = c + N * r
+
+So the matrix above becomes:
+
+    (r0, c0)->(r1, c1) => i->j: w   
+    (0, 0)  -> (0, 1)  => 0->1: 4
+    (0, 1)  -> (0, 0)  => 1->0: 2
+    ...
+ 
+
+"""
+
+
 data = """
 2413432311323
 3215453535623
@@ -16,29 +70,36 @@ data = """
 
 lines = data.splitlines()
 
-import numpy as np
-from scipy.sparse.csgraph import dijkstra
-from scipy.sparse import csr_matrix
+grid = np.array([[int(c) for c in line] for line in lines])
 
-graph = np.array([list(map(int, line)) for line in lines])
+
+N = grid.shape[0]
+graph = np.zeros((N * N, N * N))
+
+for i in range(N * N):
+    r = i // N
+    c = i % N
+    neighbors = [(r - 1, c), (r, c - 1), (r + 1, c), (r, c + 1)]
+    for r2, c2 in neighbors:
+        if r2 < 0 or r2 >= N or c2 < 0 or c2 >= N:
+            continue
+        j = c2 + N * r2
+        assert graph[i, j] == 0
+        graph[i, j] = grid[r2, c2]
+
+print(graph)
+
+#######
+
 graph = csr_matrix(graph)
+dist_matrix, predecessors = dijkstra(graph, indices=0, return_predecessors=True)
 
-dijkstra(graph, directed=False, indices=0, return_predecessors=True)
-
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csgraph.dijkstra.html
-# https://stackoverflow.com/questions/53074947/examples-for-search-graph-using-scipy
-#
-# 241
-# 321                              to node j
-# 325                           0 1 2 3 4 5 6 7 8
-#        >4     >1            +-----------------      for a grid of size N,
-#   (0)----(1)----(2)       0 | 0 4 0 3 0 0 0 0 0     it may be better to call nodes (x,y),
-#    |  <2  |  <4  |      f 1 | 2 0 1 0 2 0 0 0 0     such that connectivity is:
-#  v3|^2  v2|^4  v1|^1    r 2 | 0 4 0 0 0 1 0 0 0        (x,y) -> (x+1,y), (x,y+1), (x-1,y), (x,y-1)
-#    |  >2  |  >1  |      o 3 | 2 0 0 0 2 0 3 0 0        given x+1,y+1 < N, x-1,y-1 >= 0
-#   (3)----(4)----(5)     m 4 | 0 4 0 3 0 1 0 2 0     and then numbering the nodes i as:
-#    |  <3  |  <2  |        5 | 0 0 1 0 2 0 0 0 5        i = x + N * y
-#  v3|^3  v2|^2  v5|^1    i 6 | 0 0 0 3 0 0 0 2 0
-#    |  >2  |  >5  |        7 | 0 0 0 0 2 0 3 0 5
-#   (6)----(7)----(8)       8 | 0 0 0 0 0 1 0 2 0
-#       <3     <2
+# show path
+path = np.zeros_like(grid)
+node = N * N - 1
+while (prev := predecessors[node]) != -9999:
+    r = node // N
+    c = node % N
+    path[r, c] = 1
+    node = prev
+print(str(path).replace("0", ".").replace("1", "X"))
---
+++ day17_cheat.py
@@ -0,0 +1,32 @@
+# https://www.reddit.com/r/adventofcode/comments/18k9ne5/comment/kdqp7jx/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+
+from heapq import heappop, heappush as push
+
+G = {
+    i + j * 1j: int(c)
+    for i, r in enumerate(open("day17.txt"))
+    for j, c in enumerate(r.strip())
+}
+
+
+def f(min, max, end=[*G][-1], x=0):
+    todo = [(0, 0, 0, 1), (0, 0, 0, 1j)]
+    seen = set()
+
+    while todo:
+        val, _, pos, dir = heappop(todo)
+
+        if pos == end:
+            return val
+        if (pos, dir) in seen:
+            continue
+        seen.add((pos, dir))
+
+        for d in 1j / dir, -1j / dir:
+            for i in range(min, max + 1):
+                if pos + d * i in G:
+                    v = sum(G[pos + d * j] for j in range(1, i + 1))
+                    push(todo, (val + v, (x := x + 1), pos + d * i, d))
+
+
+print(f(1, 3), f(4, 10))
